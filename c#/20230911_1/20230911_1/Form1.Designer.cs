b9fb4b17920a210cd3d131cb0ae01997a1e98c30
szaszgergo <71765638+szaszgergo@users.noreply.github.com>
2023-09-11 22:39:55 +0200

    Add files via upload

---
+++ c#/20230911_1/20230911_1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace _20230911_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_kek = new System.Windows.Forms.Button();
+            this.btn_Kozepre = new System.Windows.Forms.Button();
+            this.btn_nagybetus = new System.Windows.Forms.Button();
+            this.btn_Fekete = new System.Windows.Forms.Button();
+            this.btn_Torol = new System.Windows.Forms.Button();
+            this.btn_Balra = new System.Windows.Forms.Button();
+            this.btn_Kisbetus = new System.Windows.Forms.Button();
+            this.btn_kiir = new System.Windows.Forms.Button();
+            this.btn_Kilep = new System.Windows.Forms.Button();
+            this.lbl_hazi = new System.Windows.Forms.Label();
+            this.btn_Jobbra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_kek
+            // 
+            this.btn_kek.Location = new System.Drawing.Point(59, 78);
+            this.btn_kek.Name = "btn_kek";
+            this.btn_kek.Size = new System.Drawing.Size(75, 23);
+            this.btn_kek.TabIndex = 0;
+            this.btn_kek.Text = "Kék";
+            this.btn_kek.UseVisualStyleBackColor = true;
+            this.btn_kek.Click += new System.EventHandler(this.btn_kek_Click);
+            // 
+            // btn_Kozepre
+            // 
+            this.btn_Kozepre.Location = new System.Drawing.Point(154, 78);
+            this.btn_Kozepre.Name = "btn_Kozepre";
+            this.btn_Kozepre.Size = new System.Drawing.Size(75, 23);
+            this.btn_Kozepre.TabIndex = 1;
+            this.btn_Kozepre.Text = "Középre";
+            this.btn_Kozepre.UseVisualStyleBackColor = true;
+            this.btn_Kozepre.Click += new System.EventHandler(this.btn_Kozepre_Click);
+            // 
+            // btn_nagybetus
+            // 
+            this.btn_nagybetus.Location = new System.Drawing.Point(248, 78);
+            this.btn_nagybetus.Name = "btn_nagybetus";
+            this.btn_nagybetus.Size = new System.Drawing.Size(75, 23);
+            this.btn_nagybetus.TabIndex = 2;
+            this.btn_nagybetus.Text = "Nagybetűs";
+            this.btn_nagybetus.UseVisualStyleBackColor = true;
+            this.btn_nagybetus.Click += new System.EventHandler(this.btn_nagybetus_Click);
+            // 
+            // btn_Fekete
+            // 
+            this.btn_Fekete.Location = new System.Drawing.Point(59, 117);
+            this.btn_Fekete.Name = "btn_Fekete";
+            this.btn_Fekete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Fekete.TabIndex = 3;
+            this.btn_Fekete.Text = "Fekete";
+            this.btn_Fekete.UseVisualStyleBackColor = true;
+            this.btn_Fekete.Click += new System.EventHandler(this.btn_Fekete_Click);
+            // 
+            // btn_Torol
+            // 
+            this.btn_Torol.Location = new System.Drawing.Point(248, 117);
+            this.btn_Torol.Name = "btn_Torol";
+            this.btn_Torol.Size = new System.Drawing.Size(75, 23);
+            this.btn_Torol.TabIndex = 5;
+            this.btn_Torol.Text = "Töröl";
+            this.btn_Torol.UseVisualStyleBackColor = true;
+            this.btn_Torol.Click += new System.EventHandler(this.btn_Torol_Click);
+            // 
+            // btn_Balra
+            // 
+            this.btn_Balra.Location = new System.Drawing.Point(59, 155);
+            this.btn_Balra.Name = "btn_Balra";
+            this.btn_Balra.Size = new System.Drawing.Size(75, 23);
+            this.btn_Balra.TabIndex = 6;
+            this.btn_Balra.Text = "Balra";
+            this.btn_Balra.UseVisualStyleBackColor = true;
+            this.btn_Balra.Click += new System.EventHandler(this.btn_Balra_Click);
+            // 
+            // btn_Kisbetus
+            // 
+            this.btn_Kisbetus.Location = new System.Drawing.Point(154, 155);
+            this.btn_Kisbetus.Name = "btn_Kisbetus";
+            this.btn_Kisbetus.Size = new System.Drawing.Size(75, 23);
+            this.btn_Kisbetus.TabIndex = 7;
+            this.btn_Kisbetus.Text = "Kisbetűs";
+            this.btn_Kisbetus.UseVisualStyleBackColor = true;
+            this.btn_Kisbetus.Click += new System.EventHandler(this.btn_Kisbetus_Click);
+            // 
+            // btn_kiir
+            // 
+            this.btn_kiir.Location = new System.Drawing.Point(248, 155);
+            this.btn_kiir.Name = "btn_kiir";
+            this.btn_kiir.Size = new System.Drawing.Size(75, 23);
+            this.btn_kiir.TabIndex = 8;
+            this.btn_kiir.Text = "Kiír";
+            this.btn_kiir.UseVisualStyleBackColor = true;
+            this.btn_kiir.Click += new System.EventHandler(this.btn_kiir_Click);
+            // 
+            // btn_Kilep
+            // 
+            this.btn_Kilep.Location = new System.Drawing.Point(154, 195);
+            this.btn_Kilep.Name = "btn_Kilep";
+            this.btn_Kilep.Size = new System.Drawing.Size(75, 23);
+            this.btn_Kilep.TabIndex = 9;
+            this.btn_Kilep.Text = "Kilép";
+            this.btn_Kilep.UseVisualStyleBackColor = true;
+            this.btn_Kilep.Click += new System.EventHandler(this.btn_Kilep_Click);
+            // 
+            // lbl_hazi
+            // 
+            this.lbl_hazi.AutoSize = true;
+            this.lbl_hazi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_hazi.Location = new System.Drawing.Point(133, 20);
+            this.lbl_hazi.Name = "lbl_hazi";
+            this.lbl_hazi.Size = new System.Drawing.Size(118, 24);
+            this.lbl_hazi.TabIndex = 10;
+            this.lbl_hazi.Text = "Házi feladat";
+            this.lbl_hazi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_hazi.Click += new System.EventHandler(this.lbl_hazi_Click);
+            // 
+            // btn_Jobbra
+            // 
+            this.btn_Jobbra.Location = new System.Drawing.Point(154, 117);
+            this.btn_Jobbra.Name = "btn_Jobbra";
+            this.btn_Jobbra.Size = new System.Drawing.Size(75, 23);
+            this.btn_Jobbra.TabIndex = 11;
+            this.btn_Jobbra.Text = "Jobbra";
+            this.btn_Jobbra.UseVisualStyleBackColor = true;
+            this.btn_Jobbra.Click += new System.EventHandler(this.btn_Jobbra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 245);
+            this.Controls.Add(this.btn_Jobbra);
+            this.Controls.Add(this.lbl_hazi);
+            this.Controls.Add(this.btn_Kilep);
+            this.Controls.Add(this.btn_kiir);
+            this.Controls.Add(this.btn_Kisbetus);
+            this.Controls.Add(this.btn_Balra);
+            this.Controls.Add(this.btn_Torol);
+            this.Controls.Add(this.btn_Fekete);
+            this.Controls.Add(this.btn_nagybetus);
+            this.Controls.Add(this.btn_Kozepre);
+            this.Controls.Add(this.btn_kek);
+            this.Name = "Form1";
+            this.Text = "Tulajdonságok állítása ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_kek;
+        private System.Windows.Forms.Button btn_Kozepre;
+        private System.Windows.Forms.Button btn_nagybetus;
+        private System.Windows.Forms.Button btn_Fekete;
+        private System.Windows.Forms.Button btn_Torol;
+        private System.Windows.Forms.Button btn_Balra;
+        private System.Windows.Forms.Button btn_Kisbetus;
+        private System.Windows.Forms.Button btn_kiir;
+        private System.Windows.Forms.Button btn_Kilep;
+        private System.Windows.Forms.Label lbl_hazi;
+        private System.Windows.Forms.Button btn_Jobbra;
+    }
+}
+
---
+++ c#/20230911_1/20230911_1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace _20230911_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool isLowerCase = false;
+        private bool isUpperCase = false;
+
+        private string originalLabelText;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            originalLabelText = lbl_hazi.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Kilep_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ki szeretnél lépni?","Kilépés", MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+
+            }
+            
+        }
+
+        private void btn_kek_Click(object sender, EventArgs e)
+        {
+            Color color = Color.FromArgb(45, 39, 226);
+            lbl_hazi.ForeColor = color;
+
+
+        }
+
+        private void btn_Fekete_Click(object sender, EventArgs e)
+        {
+            Color color = Color.FromArgb(0, 0, 0);
+            lbl_hazi.ForeColor = color;
+        }
+
+        private void btn_Kozepre_Click(object sender, EventArgs e)
+        {
+            int newX = 135;
+            int newY = 20;
+            lbl_hazi.Location = new Point(newX, newY);
+        }
+
+        private void lbl_hazi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_nagybetus_Click(object sender, EventArgs e)
+        {
+            string txt1 = lbl_hazi.Text;
+            lbl_hazi.Text = txt1.ToUpper();
+
+
+                  if (isUpperCase)
+            {
+                lbl_hazi.Text = originalLabelText;
+                isUpperCase = false;
+            }
+            else
+            {
+                lbl_hazi.Text = lbl_hazi.Text.ToUpper();
+                isUpperCase = true;
+            }
+        }
+
+        private void btn_Kisbetus_Click(object sender, EventArgs e)
+        {
+            string txt1 = lbl_hazi.Text;
+            lbl_hazi.Text = txt1.ToLower();
+
+            if (isLowerCase)
+            {
+                lbl_hazi.Text = originalLabelText;
+                isLowerCase = false;
+            }
+            else
+            {
+                lbl_hazi.Text = lbl_hazi.Text.ToLower();
+                isLowerCase = true;
+            }
+
+        }
+
+        private void btn_Torol_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ki szeretnéd törölni?", "Törlés", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                lbl_hazi.Text = "";
+            }
+            else
+            {
+
+            }
+        }
+
+        private void btn_kiir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Házi feladat ", "Kiíratás");
+        }
+
+        private void btn_Balra_Click(object sender, EventArgs e)
+        {
+            // Define new X and Y coordinates for the label's position
+            int newX = 0; // Replace with your desired X-coordinate
+            int newY = 20; // Replace with your desired Y-coordinate
+
+            // Update the label's position
+            lbl_hazi.Location = new Point(newX, newY);
+        }
+
+        private void btn_Jobbra_Click(object sender, EventArgs e)
+        {
+         
+            int newX = 230;
+            int newY = 20;
+            lbl_hazi.Location = new Point(newX, newY);
+        }
+    }
+}
