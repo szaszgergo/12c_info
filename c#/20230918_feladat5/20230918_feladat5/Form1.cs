449b5b15b83f27925eab4d1f43c8dc6a27a08cff
szaszgergo <71765638+szaszgergo@users.noreply.github.com>
2023-09-18 18:49:37 +0200

    Add files via upload

---
+++ c#/20230918_feladat5/20230918_feladat5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace _20230918_feladat5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Kereset_szoveg = textBox1.Text.ToLower();
+
+            // Ideiglenes lista az új eredmények tárolásához
+            List<string> ujElemek = new List<string>();
+
+            foreach (string item in listBox1.Items)
+            {
+                if (item.ToLower().Contains(Kereset_szoveg))
+                {
+                    ujElemek.Add(item);
+                }
+            }
+
+            // Töröljük a korábbi kiválasztásokat
+            listBox1.ClearSelected();
+
+            // Másoljuk az új eredményeket a SelectedItems listába
+            foreach (string ujElem in ujElemek)
+            {
+                int index = listBox1.Items.IndexOf(ujElem);
+                if (index >= 0)
+                {
+                    listBox1.SetSelected(index, true);
+                }
+            }
+        }
+    }
+}
